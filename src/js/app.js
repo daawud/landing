085a45802cb58a0e53e14fd66d8ff64baf4834ca
romemor <romemor87@gmail.com>
2020-02-16 00:40:54 +0300

    Init

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import '../scss/app.scss';
+import '../pug/index.pug';
+
+import '@babel/polyfill';
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+console.log('Hi, my name is Ukyo!'); // eslint-disable-line no-console
